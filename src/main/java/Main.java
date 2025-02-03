46b4029326c34212ecebbb97a7cdbaa5701c3084
Bigtoyka <ru.gosha@mail.ru>
2025-02-03 23:46:36 +0300

    add new functional

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,51 @@ import java.util.*;
 
 public class Main {
     private static void start() {
+        int life = 0;
+        Guillotine guillotine = new Guillotine();
         System.out.println("Начать игру или выйыти? [Y,N]");
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String answer = scanner.nextLine();
-//            if (answer.equals("N")) {
-//                break;
-//            }
-            String word = getWord();
-            System.out.println(word);
-            slideWord(word);
+        String answer = scanner.nextLine();
+        String word = getWord();
+        slideWord(word);
+        while (life <= 7) {
+            System.out.print("Введите букву: ");
+            String answerChar = scanner.nextLine();
+            if (!word.contains(answerChar)) {
+                System.out.println("Вы не отгдадили букву");
+                switch (life) {
+                    case 0:
+                        guillotine.firstMissTake();
+                        break;
+                    case 1:
+                        guillotine.secondMissTake();
+                        break;
+                    case 2:
+                        guillotine.threeMissTake();
+                        break;
+                    case 3:
+                        guillotine.forMissTake();
+                        break;
+                    case 4:
+                        guillotine.fiveMissTake();
+                        break;
+                    case 5:
+                        guillotine.sixMissTake();
+                        break;
+                    case 6:
+                        guillotine.sevenMissTake();
+                        break;
+                    case 7:
+                        System.out.println("Вы проиграли");
+                        break;
+                }
+                life++;
+                continue;
+            }
+            System.out.println("Вы отгадали букву: " + answerChar);
+
         }
-//        System.out.println("Игра окончена");
+
     }
 
     private static String getWord() {
@@ -34,14 +67,16 @@ public class Main {
         String myWord = words.get(random.nextInt(words.size()));
         return myWord;
     }
+
     private static void slideWord(String word) {
         int countSlide = word.length();
         String[] slideWords = new String[countSlide];
         for (int i = 0; i < countSlide; i++) {
             slideWords[i] = "*";
         }
-        System.out.println(slideWords.toString());
+        Arrays.stream(slideWords).forEach(System.out::print);
     }
+
     public static void main(String[] args) {
         start();
     }
